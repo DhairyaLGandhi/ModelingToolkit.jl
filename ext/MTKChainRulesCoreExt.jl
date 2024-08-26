e955cc4ca7263ffd7c51b84140592372722f8521
DhairyaLGandhi <dhairya@juliacomputing.com>
2024-08-26 19:41:42 +0530

    chore: add dispatch for vector tangent

--- ext/MTKChainRulesCoreExt.jl
+++ ext/MTKChainRulesCoreExt.jl
@@ -6,8 +6,11 @@ import ChainRulesCore: NoTangent
 
 function ChainRulesCore.rrule(::Type{MTK.MTKParameters}, tunables, args...)
     function mtp_pullback(dt)
-        (NoTangent(), dt.tunable[1:length(tunables)],
-            ntuple(_ -> NoTangent(), length(args))...)
+        t = hasproperty(dt, :tunables) ? dt.tunables : dt[1]
+        (NoTangent(), t[1:length(tunables)], ntuple(_ -> NoTangent(), length(args))...)
+    end
+    function mtp_pullback(dt::AbstractArray)
+        (NoTangent(), dt[1:length(tunables)], ntuple(_ -> NoTangent(), length(args))...)
     end
     MTK.MTKParameters(tunables, args...), mtp_pullback
 end
